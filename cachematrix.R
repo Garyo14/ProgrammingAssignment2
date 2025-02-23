664dce12e8fefb0994e03866c81406407ebf04a8
Garyo14 <tgrhryow14@gmail.com>
2025-02-23 09:41:52 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,29 @@
 
 ## Write a short comment describing this function
 
+## makeCacheMatrix: Membuat objek matriks khusus yang bisa menyimpan inversnya
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL  # Menyimpan nilai invers
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL  # Reset cache jika matriks berubah
+  }
+  get <- function() x  # Mengambil matriks
+  setInverse <- function(inverse) inv <<- inverse  # Menyimpan invers
+  getInverse <- function() inv  # Mengambil invers dari cache
+  
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve: Menghitung atau mengambil invers dari cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("Mengambil dari cache")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)  # Menghitung invers
+  x$setInverse(inv)  # Menyimpan ke cache
+  inv
 }
